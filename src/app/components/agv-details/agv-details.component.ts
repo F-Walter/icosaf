16401c496a96244d37ba028c6bc65ceee535b53a
Claudio Filocamo <s256700@studenti.polito.it>
2020-08-17 16:24:05 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/app/components/agv-details/agv-details.component.ts
+++ src/app/components/agv-details/agv-details.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+import { MatDialog } from '@angular/material/dialog';
+import { ProblemModalComponent } from '../UCDetails/modal/problem-modal.component';
 import { MatTableDataSource } from '@angular/material/table';
+
+export interface Item {
+  state: number; // 0 nessun problema or problema risolto // 1 problema // 2 componente non ancora considerato //3 loading
+  id: string;
+  kit: string;
+  hour: string;
+  problemsFound: string;
+  description: string;
+  button: string;
+}
 
 
 interface PrelieviInterface {
@@ -14,9 +27,20 @@ interface PrelieviInterface {
 @Component({
   selector: 'app-agv-details',
   templateUrl: './agv-details.component.html',
-  styleUrls: ['./agv-details.component.css']
+  styleUrls: ['./agv-details.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ]
 })
 export class AgvDetailsComponent implements OnInit {
+  dataSource = PROBLEMS;
+  columnsToDisplay = ['state', 'id', 'kit', 'problemsFound', 'button', 'hour'];
+  expandedElement: Item | null;
+  constructor(public dialog: MatDialog) { }
 
   displayedColumnsPrelievi: string[] = ['state', 'components', 'kit', 'hours'];
   dataSourcePrelievi: MatTableDataSource<PrelieviInterface>
@@ -33,4 +57,39 @@ export class AgvDetailsComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  
+  headerOfColumn(column: string) {
+    switch (column) {
+      case 'state':
+        return 'Stato';
+      case 'id':
+        return 'Componente';
+      case 'hour':
+        return 'Ora';
+      case 'problemsFound':
+        return 'Problemi Rilevati';
+      default:
+        break;
+    }
+
+  }
+  solve(element: Item){
+    event.stopPropagation();
+    this.dialog.open(ProblemModalComponent);
+  }
+  
 }
+
+const PROBLEMS: Item[] = [
+  {
+    state: 1,
+    id: 'Oxygen',
+    kit : 'PIPPO',
+    hour: '15.9994',
+    problemsFound: 'Tipologia Errore',
+    button:'',
+    description: `Oxygen is a chemical element with problemsFound O and atomic number 8. It is a member of
+         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
+         agent that readily forms oxides with most elements as well as with other compounds.`
+  },
+]
